4b1580ba7d802451d8d665d39b05d51dc2b5de36
inflowsuite-crypto <inflowsuite@gmail.com>
2025-11-22 03:36:57 -0600

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -126,7 +126,7 @@ export default function Footer({ onNavigate }: FooterProps) {
           <img
             src={social.src}
             alt={social.label}
-            className="h-8 w-12 object-contain" 
+            className="h-8 w-22 object-contain" 
           />
         </a>
       ))}
